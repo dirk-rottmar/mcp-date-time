ebbf2a25bd2ed43e28206075521921ab65cd734c
Dirk Rottmar <dirk@rottmar.net>
2026-04-06 23:12:07 +0700

    add docker files

--- src/test/java/net/rottmar/ai/mcpserver/datetime/DateTimeApplicationTests.java
+++ src/test/java/net/rottmar/ai/mcpserver/datetime/DateTimeApplicationTests.java
@@ -3,7 +3,7 @@ package net.rottmar.ai.mcpserver.datetime;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProfilesGeneratorApplicationTests {
+class DateTimeApplicationTests {
 
 
 //	@Test
